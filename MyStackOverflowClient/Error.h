1b9f23246b21c27609bbc92ee3150e83dfac772e
Alberto-Vega <el_vega@live.com>
2015-12-07 17:32:09 -0800

    Added file for Error

---
+++ MyStackOverflowClient/Error.h
@@ -0,0 +1,13 @@
+//
+//  Error.h
+//  MyStackOverflowClient
+//
+//  Created by Alberto Vega Gonzalez on 12/7/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Error : NSString
+
+@end
---
+++ MyStackOverflowClient/Error.m
@@ -0,0 +1,13 @@
+//
+//  Error.m
+//  MyStackOverflowClient
+//
+//  Created by Alberto Vega Gonzalez on 12/7/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#import "Error.h"
+
+@implementation Error
+
+@end
