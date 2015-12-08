e7200f05e82afd39ab88fda16ae25a90522f4ee4
Alberto-Vega <el_vega@live.com>
2015-12-07 16:18:27 -0800

    Added container View Controller

--- MyStackOverflowClient/MenuContainerViewController.h
+++ MyStackOverflowClient/MenuContainerViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MenuContainerViewController.h
 //  MyStackOverflowClient
 //
 //  Created by Alberto Vega Gonzalez on 12/7/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MenuContainerViewController : UIViewController
 
 @end
-
