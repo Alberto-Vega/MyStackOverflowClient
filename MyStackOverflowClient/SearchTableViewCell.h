827aeb24a5948a7b1ea69b0f32296cf8f31c3bf8
Alberto-Vega <el_vega@live.com>
2015-12-09 17:50:45 -0800

    Moved Cell file to the Controllers folder

---
+++ MyStackOverflowClient/SearchTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchTableViewCell.h
+//  
+//
+//  Created by Alberto Vega Gonzalez on 12/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchTableViewCell : UITableViewCell
+
+@end
---
+++ MyStackOverflowClient/SearchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SearchTableViewCell.m
+//  
+//
+//  Created by Alberto Vega Gonzalez on 12/9/15.
+//
+//
+
+#import "SearchTableViewCell.h"
+
+@implementation SearchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
